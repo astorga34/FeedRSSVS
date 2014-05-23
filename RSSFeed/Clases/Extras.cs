ca804322a21a92e3a875a775e7ddde12439ea403
astorga34 <alejandroadam34@gmail.com>
2014-05-22 20:29:14 -0600

    Se creo una nueva clase para alojar las funciones que se haran cargo de validar la existencia de la base de datos

---
+++ RSSFeed/Clases/Extras.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlServerCe;
+
+namespace RSSFeed.Clases
+{
+    class Extras
+    {
+        public static Exception msj;
+        public static bool existe_db()
+        {
+            msj = null;
+            try
+            {
+                string connectionString = "DataSource=\"test.sdf\";";
+                SqlCeEngine en = new SqlCeEngine(connectionString);
+                if (!en.Verify())
+                {
+                    en.CreateDatabase();
+                }
+                en.Dispose();
+            }
+            catch (Exception f)
+            {
+                msj = f;
+            }
+            return false;
+        }
+    }
+}
